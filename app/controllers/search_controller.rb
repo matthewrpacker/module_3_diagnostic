460431471f0cad5518aff5c8b3089895bb2dd93b
matthewrpacker <matthewrpacker@gmail.com>
2016-09-12 10:06:24 -0600

    Begin adding search model

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,16 @@
 class SearchController < ApplicationController
   def index
     response = Faraday.get "https://api.data.gov/nrel/alt-fuel-stations/v1.json?limit=1&api_key=#{ENV['NREL_KEY']}"
-    @parsed_response = JSON.parse(response.body)
+    @parsed_response = JSON.parse(response.body)['fuel_stations']
+    @parsed_response.map do |pr|
+      Search.new(pr)
+      byebug
+    end
+  end
+end
+
+class Search
+  def initialize(response_hash)
+    @zip = response_hash['zip']
   end
 end
