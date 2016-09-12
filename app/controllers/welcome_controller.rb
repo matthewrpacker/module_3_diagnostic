bdd46823f148cdceae8848b9592545dbaab5f4e4
matthewrpacker <matthewrpacker@gmail.com>
2016-09-12 09:21:20 -0600

    Add Faraday connection to welcome controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    
+    @connection = Faraday.get "https://kM71RdxKpMZmTXWP3q36xEAGSruLqKmiMcBxSKKB@api.data.gov/nrel/alt-fuel-stations/v1.json?limit=1"
   end
 end
