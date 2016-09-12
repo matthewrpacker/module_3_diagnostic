c1b34672f202fc7cf4df3294f4746c85583f686e
matthewrpacker <matthewrpacker@gmail.com>
2016-09-12 10:40:17 -0600

    Add zip to search index view

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,20 +2,16 @@ class SearchController < ApplicationController
   def index
     response = Faraday.get "https://api.data.gov/nrel/alt-fuel-stations/v1.json?limit=1&api_key=#{ENV['NREL_KEY']}"
     @parsed_response = JSON.parse(response.body)['fuel_stations']
-    @searcher = @parsed_response.map do |pr|
-      Searcher.new(pr)
+    @zip = @parsed_response.map do |pr|
+      Searcher.new(pr).zip
     end
-    @searcher.zip
-    @searcher.
-    byebug
   end
 end
 
 class Searcher
-  attr_reader :zip, :station_name
+  attr_reader :zip
 
   def initialize(response_hash)
     @zip = response_hash['zip']
-    @station_name = response_hash['station_name']
   end
 end
