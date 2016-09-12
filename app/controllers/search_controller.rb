20d3cb9199717b46f1db291aed2d6b41d7edb4be
matthewrpacker <matthewrpacker@gmail.com>
2016-09-12 09:51:07 -0600

    Add search controller with index action and view

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,6 @@
+class SearchController < ApplicationController
+  def index
+    response = Faraday.get "https://api.data.gov/nrel/alt-fuel-stations/v1.json?limit=1&api_key=#{ENV['NREL_KEY']}"
+    @parsed_response = JSON.parse(response.body)
+  end
+end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,4 @@
 class WelcomeController < ApplicationController
   def index
-    response = Faraday.get "https://api.data.gov/nrel/alt-fuel-stations/v1.json?limit=1&api_key=#{ENV['NREL_KEY']}"
-    @parsed_response = JSON.parse(response.body)
   end
 end
