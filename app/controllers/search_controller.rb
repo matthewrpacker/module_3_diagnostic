d6d112911903eb7f71339abdd093fddf32213ded
matthewrpacker <matthewrpacker@gmail.com>
2016-09-12 10:21:30 -0600

    Add Searcher PORO inside search controller file

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,15 +2,20 @@ class SearchController < ApplicationController
   def index
     response = Faraday.get "https://api.data.gov/nrel/alt-fuel-stations/v1.json?limit=1&api_key=#{ENV['NREL_KEY']}"
     @parsed_response = JSON.parse(response.body)['fuel_stations']
-    @parsed_response.map do |pr|
-      Search.new(pr)
-      byebug
+    @searcher = @parsed_response.map do |pr|
+      Searcher.new(pr)
     end
+    @searcher.zip
+    @searcher.
+    byebug
   end
 end
 
-class Search
+class Searcher
+  attr_reader :zip, :station_name
+
   def initialize(response_hash)
     @zip = response_hash['zip']
+    @station_name = response_hash['station_name']
   end
 end
